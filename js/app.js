c7517ff5e92a069d28d0c9af4b49fad3736b93b7
massi-17 <massimianisilvia@gmail.com>
2022-02-04 16:21:02 +0100

    settato a 2 decimali

--- js/app.js
+++ js/app.js
@@ -6,14 +6,18 @@
 // se no prezzo pieno
 // inserire nell'html il prezzo finale con massimo due decimali
 
-const km = prompt('Quanti chilometri devi percorrere?');
-console.log(km);
-const anni = prompt('Quanti anni hai?');
+
+let km = prompt('Quanti chilometri devi percorrere?') * 1;
+
+const anni = prompt('Quanti anni hai?') * 1;
 console.log(anni);
 
 const prezzoKm = 0.21;
 
-let prezzoBase = km * prezzoKm;
+let prezzoBase = (km * prezzoKm) * 1;
+
+let message
+
 
 
 
@@ -28,4 +32,7 @@ if (anni < 18) {
 
 
 
+
 console.log(totale);
+totale = totale.toFixed(2);
+document.getElementById('totale_prezzo').innerHTML += totale;
